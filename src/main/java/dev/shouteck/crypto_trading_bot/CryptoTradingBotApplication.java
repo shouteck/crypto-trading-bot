d986a21688a45f84d35ac6087b5d48eac467d21a
shou-teck <ShouTeck.Ting@riotinto.com>
2025-01-15 09:38:07 +0800

    initial commit

---
+++ src/main/java/dev/shouteck/crypto_trading_bot/CryptoTradingBotApplication.java
@@ -0,0 +1,13 @@
+package dev.shouteck.crypto_trading_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/shouteck/crypto_trading_bot/CryptoTradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shouteck.crypto_trading_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoTradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
