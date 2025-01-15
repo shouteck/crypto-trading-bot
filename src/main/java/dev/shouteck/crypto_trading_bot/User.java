461982c4a78619b4afccd3237a9ab429f3b9b820
shou-teck <ShouTeck.Ting@riotinto.com>
2025-01-15 17:31:35 +0800

    added User Entity that represents users with a single wallet

---
+++ src/main/java/dev/shouteck/crypto_trading_bot/User.java
@@ -0,0 +1,54 @@
+package dev.shouteck.crypto_trading_bot;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private BigDecimal walletBalance;
+
+    public User() {}
+
+    public User(String username, BigDecimal walletBalance) {
+        this.username = username;
+        this.walletBalance = walletBalance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public BigDecimal getWalletBalance() {
+        return walletBalance;
+    }
+
+    public void setWalletBalance(BigDecimal walletBalance) {
+        this.walletBalance = walletBalance;
+    }
+}
